14f6e68fa3ff39f32f7c4aa892fdcfdd2d48469a
Ericka Amador <vanessaamador830@gmail.com>
2022-04-22 18:06:47 -0600

    Avance 2
    Hasta 29 de marzo

---
+++ ProyectoBlazor/Modelos/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos;
+
+public class Producto
+{
+    [Required(ErrorMessage = "El campo codigo es obligatorio")]
+    public string Codigo { get; set; }
+    [Required(ErrorMessage = "El campo descripcion es obligatorio")]
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public int Existencia { get; set; }
+    public byte[] Imagen { get; set; }
+}
